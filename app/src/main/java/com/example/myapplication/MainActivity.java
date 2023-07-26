43544efa9c48de625ae893247c8e3ee97eb08e12
devandropedia <admin@devandro.in>
2023-07-26 11:42:31 +0530

    new

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.shreyaspatil.EasyUpiPayment.EasyUpiPayment;
+import com.example.mylibrary.ToasterMessage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EasyUpiPayment easyUpiPayment = new EasyUpiPayment.Builder()
-                .with(this)
-                .setPayeeVpa("shreyaspatil@upi")
-                .setPayeeName("Shreyas Patil")
-                .setTransactionId("20190603022401")
-                .setTransactionRefId("0120192019060302240")
-                .setDescription("For Today's Food")
-                .setAmount("90.00")
-                .build();
+        ToasterMessage.s(getApplicationContext(),"Hello");
+
     }
 }
